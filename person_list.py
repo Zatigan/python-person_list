caff204c3c0a87ed0ec937848181aefa84227f96
BOULET Pierre <pierre.boulet@maif.fr>
2026-05-18 14:08:40 +0200

    feat: 2nd option to generate fake people with a single loop

--- person_list.py
+++ person_list.py
@@ -3,10 +3,18 @@ from faker import Faker
 
 fake = Faker("fr_FR")
 
-# En générant les données directement lors de la génération du tableau
-print(tabulate({"Nom": [fake.name() for i in range(10)],
-                "Email": [fake.email() for i in range(10)],
-                "Ville": [fake.city() for i in range(10)]}, headers="keys", tablefmt="outline"))
+# En générant les données directement lors de la génération du tableau (peut-être pas fou en terme d'éco-conception)
+# ===================================================================================================================
+# print(tabulate({"Nom": [fake.name() for i in range(10)],
+#                 "Email": [fake.email() for i in range(10)],
+#                 "Ville": [fake.city() for i in range(10)]}, headers="keys", tablefmt="outline"))
 
 
 # En créant un tableau et en insérant avec une boucle les données dans ce même tableau
+# =====================================================================================
+fakePeople = []
+
+for i in range(10):
+ fakePeople.append([fake.name(), fake.email(), fake.city()])
+
+print(tabulate(fakePeople, headers=["Nom", "Email", "Ville"], tablefmt="grid"))
